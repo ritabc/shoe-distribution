f6de5d159facd0ef41d95e00a1db9afd5cb96772
ritabc <ritabc@gmail.com>
2018-07-20 10:18:17 -0700

    Pass tests for uniquness validation

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,11 @@
 require('spec_helper')
 
 describe(Brand) do
-  it {should have_and_belong_to_many(:stores)}
+  context('associations') do
+     it {should have_and_belong_to_many(:stores)}
+  end
+
+  context('validation for uniquness') do
+    it { should validate_uniqueness_of(:brand_name)}
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,5 +1,11 @@
 require('spec_helper')
 
 describe(Store) do
-  it {should have_and_belong_to_many(:brands)}
+  context('associations') do
+    it {should have_and_belong_to_many(:brands)}
+  end
+
+  context('validation for uniquness') do
+    it { should validate_uniqueness_of(:store_name)}
+  end
 end
