43a9407d682ad7547373fdc9370140ee141be378
ritabc <ritabc@gmail.com>
2018-07-20 11:40:45 -0700

    Pass test for uniqueness and for positive price numbers

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,12 +1,18 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+
   validates_presence_of(:brand_name)
   validates_uniqueness_of(:brand_name)
-  before_save(:upcase_name)
-
+  validates_numericality_of :price, :only_integer => false, :greater_than_or_equal_to => 0
+  before_validation(:upcase_name)
+  
 private
 
   def upcase_name
     self.brand_name=(brand_name.titleize)
   end
+
+  # def convert_num_to_price
+  #   self.price
+  # end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -5,14 +5,30 @@ describe(Brand) do
      it {should have_and_belong_to_many(:stores)}
   end
 
-  context('validation for uniquness') do
-    it { should validate_uniqueness_of(:brand_name)}
+  context('validation for uniqueness') do
+    it('will not add another duplicate brand') do
+      new_brand = Brand.create({:brand_name => 'Crocs', :price => 5})
+      new_brand2 = Brand.create({:brand_name => 'Crocs', :price => 5})
+      expect(Brand.all).to(eq([new_brand]))
+    end
   end
+  #
+  # context('validation for uniquness') do
+  #   binding.pry
+  #   it { should validate_uniqueness_of(:brand_name)}
+  # end
 
-  context('titlize functionality with before_save callback') do
+  context('titleize functionality with before_save callback') do
     it('will capitalize every word before saving') do
       new_brand = Brand.create({:brand_name => 'chucks shoes'})
       expect(new_brand.brand_name).to(eq('Chucks Shoes'))
+    end
+  end
+
+  context('validates price input for positive numbers') do
+    it('will not save if price if negative') do
+      new_brand = Brand.create({:brand_name => 'chucks shoes', :price => -4})
+      expect(Brand.all).to(eq([]))
     end
   end
 end
